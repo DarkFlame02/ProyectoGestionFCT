7fc4484f2a7fdcc020487eca8753980cc19954e3
GonAfMont <157701843+GonAfMont@users.noreply.github.com>
2024-11-22 13:44:46 +0000

    controller+View
    Funciones básicas de ambos

--- src/main/java/aed/ui/GestionFCTApp.java
+++ src/main/java/aed/ui/GestionFCTApp.java
@@ -1,4 +1,17 @@
 package aed.ui;
 
-public class GestionFCTApp {
+import aed.ui.controller.RootController;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class GestionFCTApp extends Application {
+    RootController rootController = new RootController();
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Gestión FCT");
+        primaryStage.setScene(new Scene(rootController.getRoot()));
+        primaryStage.show();
+    }
 }
--- src/main/java/aed/ui/controller/RootController.java
+++ src/main/java/aed/ui/controller/RootController.java
@@ -1,4 +1,84 @@
 package aed.ui.controller;
 
-public class RootController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.BorderPane;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class RootController implements Initializable {
+    @FXML
+    private Tab alumnoTab;
+
+    @FXML
+    private Tab tutorTab;
+
+    @FXML
+    private Tab comentTab;
+
+    @FXML
+    private Tab empresaTab;
+
+    @FXML
+    private Tab practicasTab;
+
+    @FXML
+    private BorderPane root;
+
+    @FXML
+    private TabPane selectionTabPane;
+
+    @FXML
+    private Tab visitasTab;
+
+    public RootController() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/RootView.fxml"));
+            loader.setController(this);
+            loader.load();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    public BorderPane getRoot() {
+        return root;
+    }
+
+    @FXML
+    void onAddAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onCloseAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onDeleteAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onSearchAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onUpdateAction(ActionEvent event) {
+
+    }
+
 }
