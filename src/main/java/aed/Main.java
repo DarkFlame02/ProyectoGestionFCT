07e908d4b3343548c905855bec74a8751ce4e97b
GonAfMont <157701843+GonAfMont@users.noreply.github.com>
2024-11-22 13:49:55 +0000

    Main
    main

--- src/main/java/aed/Main.java
+++ src/main/java/aed/Main.java
@@ -1,4 +1,11 @@
 package aed;
 
+import aed.ui.GestionFCTApp;
+import javafx.application.Application;
+
 public class Main {
+    public static void main(String[] args) {
+        Application.launch(GestionFCTApp.class, args);
+    }
+
 }
