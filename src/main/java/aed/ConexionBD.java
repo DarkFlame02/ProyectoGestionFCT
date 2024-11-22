f85adee0242d3257ea1ae53b7ca98cc595c69ad5
Ismael Medina <104896885+DarkFlame02@users.noreply.github.com>
2024-11-22 12:09:15 +0000

    Alumno

--- src/main/java/aed/ConexionBD.java
+++ src/main/java/aed/ConexionBD.java
@@ -1,4 +1,4 @@
-package dad;
+package aed;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
---
+++ src/main/java/aed/Main.java
@@ -0,0 +1,7 @@
+package aed;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
