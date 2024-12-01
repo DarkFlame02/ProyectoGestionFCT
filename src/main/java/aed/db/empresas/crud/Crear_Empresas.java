88f0a9e12416991858b33d487da247554fcbc322
Ismael Medina <104896885+DarkFlame02@users.noreply.github.com>
2024-12-01 21:20:32 +0000

    Terminado

--- src/main/java/aed/db/empresas/crud/Crear_Empresas.java
+++ src/main/java/aed/db/empresas/crud/Crear_Empresas.java
@@ -11,27 +11,26 @@ public class Crear_Empresas {
 
     public void registrarEmpresa(String nifEmpresa, String nombreEmpresa, String direccionEmpresa, String tipoEmpresa,
                                  String nombreTutorE, String apellidosTutorE, String telefonoContacto, String emailTutorE) {
+
         String insertEmpresaQuery = "INSERT INTO empresa (nifEmpresa, nombreEmpresa, direccionEmpresa, tipoEmpresa) VALUES (?, ?, ?, ?)";
         String insertTutorQuery = "INSERT INTO tutorempresa (nombreTutorE, apellidosTutorE, telefonoContacto, emailTutorE, idEmpresa) VALUES (?, ?, ?, ?, ?)";
 
         try (Connection conn = ConexionHCP.getConnection()) {
-            // Desactiva el auto-commit para manejar ambas operaciones como una sola transacción
             conn.setAutoCommit(false);
 
             try (PreparedStatement empresaStmt = conn.prepareStatement(insertEmpresaQuery, PreparedStatement.RETURN_GENERATED_KEYS)) {
-                // Inserta en la tabla empresa
+
                 empresaStmt.setString(1, nifEmpresa);
                 empresaStmt.setString(2, nombreEmpresa);
                 empresaStmt.setString(3, direccionEmpresa);
                 empresaStmt.setString(4, tipoEmpresa);
                 empresaStmt.executeUpdate();
 
-                // Recupera el ID generado automáticamente
                 ResultSet generatedKeys = empresaStmt.getGeneratedKeys();
                 if (generatedKeys.next()) {
+
                     int empresaId = generatedKeys.getInt(1);
 
-                    // Inserta en la tabla TutorE_Primaria usando el ID de empresa
                     try (PreparedStatement tutorStmt = conn.prepareStatement(insertTutorQuery)) {
                         tutorStmt.setString(1, nombreTutorE);
                         tutorStmt.setString(2, apellidosTutorE);
